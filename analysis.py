c7acb77d8f0eb0468459faf8608d206eb06715e7
Jonathan Tsai <modopo@gmail.com>
2017-10-18 01:37:26 -0700

    PEP8 stylized

--- analysis.py
+++ analysis.py
@@ -2,14 +2,16 @@
 
 import psycopg2
 
+
 def connect():
     """Connects to PSQL database. Returns connection if successful"""
     try:
-        db = psycopg2.connect(database = "news")
+        db = psycopg2.connect(database="news")
         c = db.cursor()
         return db, c
-    except:
-        print("Connection unsuccessful!")
+    except psycopg2.Error as e:
+        print("Connection unsuccessful: " + e.pgerror)
+
 
 def process(query):
     """Performs query and returns result"""
@@ -18,6 +20,7 @@ def process(query):
     result = c.fetchall()
     db.close()
     return result
+
 
 def get_popular_articles():
     """Prints the top 3 articles of all time"""
@@ -33,13 +36,14 @@ def get_popular_articles():
     for x in result:
         print('"' + x[0] + '" - ' + str(x[1]))
 
+
 def get_popular_author():
     """Prints the authors' popularity by page views"""
     result = process("""select authors.name, count(*) as view
                             from articles, authors, log
-                                where log.path like '%' || articles.slug || '%'
-                                and log.status = '200 OK'
-                                and articles.author = authors.id
+                            where log.path like '%' || articles.slug || '%'
+                            and log.status = '200 OK'
+                            and articles.author = authors.id
                             group by authors.name
                             order by view desc""")
 
@@ -47,28 +51,31 @@ def get_popular_author():
     for x in result:
         print(x[0] + " - " + str(x[1]) + " views")
 
+
 def get_error_rate():
     """Prints the days that have more than 1% error rate"""
-    result = process("""select to_char(failed.date, 'Mon DD, YYYY'), failed.fcount, success.scount
-                            from (select time::date as date, count(status) as fcount
-                                    from log
-                                        where status != '200 OK'
-                                    group by date) as failed
+    result = process("""select to_char(failed.date, 'Mon DD, YYYY'), 
+                            failed.fcount, success.scount
+                            from (select time::date as date, 
+                            count(status) as fcount
+                            from log
+                            where status != '200 OK'
+                            group by date) as failed
                             full outer join
                             (select time::date as date, count(status) as scount
-                                from log
-                                    where status = '200 OK'
-                                group by date) as success
+                            from log 
+                            where status = '200 OK'
+                            group by date) as success
                             on failed.date = success.date
-                                where fcount * 100 >= (scount + fcount)""")
+                            where fcount * 100 >= (scount + fcount)""")
 
-    print("\n\nThese are the day(s) that have more than 1% of requests lead to error: \n")
+    print("\n\nThese are the day(s) that "
+          "have more than 1% of requests lead to error: \n")
     for x in result:
-        error = float(x[1])/(float(x[1]) + float(x[2]))
-        print(x[0] + ' - ' + str(round(100*error, 2)) + '% error')
+        error = float(x[1]) / (float(x[1]) + float(x[2]))
+        print(x[0] + ' - ' + str(round(100 * error, 2)) + '% error')
+
 
 get_popular_articles()
 get_popular_author()
 get_error_rate()
-
-
